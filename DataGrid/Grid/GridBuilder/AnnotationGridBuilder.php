1f0171e1c4acd0fb6abe093bab4498077db34c2a
Krzysztof Januś <fluke.kuczwa@gmail.com>
2014-11-03 16:48:24 +0100

    fix

--- DataGrid/Grid/GridBuilder/AnnotationGridBuilder.php
+++ DataGrid/Grid/GridBuilder/AnnotationGridBuilder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Tutto\Bundle\DataGridBundle\DataGrid\Grid\GridBuilder;
+use Symfony\Component\Intl\Exception\NotImplementedException;
 
 /**
  * Class AnnotationGridBuilder
@@ -11,6 +12,6 @@ class AnnotationGridBuilder extends AbstractGridBuilder {
      * @return void
      */
     public function build() {
-
+        throw new NotImplementedException("AnnotationGridBuilder is not implemented yet.");
     }
 }
